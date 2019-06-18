87bfb847cb137c6aa681a23a15d34e130487b7ba
Hanleyjames <hanley.doth@outlook.com>
2019-06-18 11:50:01 -0700

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { HomeComponent } from './component/home/home.component';
+import { ResultsComponent } from './component/results/results.component';
+import { LoginComponent } from './component/login/login.component';
+import { SearchComponent } from './component/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ResultsComponent,
+    LoginComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
