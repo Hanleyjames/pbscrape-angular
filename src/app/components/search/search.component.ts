fb2028bb2656054431e91dad3fa278785082176f
Hanleyjames <hanley.doth@outlook.com>
2019-06-21 16:44:02 -0700

    spaz

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-search',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
+  keywordSearch: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.keywordSearch = this.fb.group({
+      keyword: ''
+    })
   }
 
 }
