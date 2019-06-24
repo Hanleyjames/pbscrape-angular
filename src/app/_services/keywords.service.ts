b060aba281789c08bff4dd887ea528daefe8fbe3
Hanleyjames <hanley.doth@outlook.com>
2019-06-24 14:12:13 -0700

    Add more to keyword component

--- src/app/_services/keywords.service.ts
+++ src/app/_services/keywords.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Keyword} from '../_models/keywords';
+import {BehaviorSubject} from 'rxjs';
+import {distinctUntilChanged} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class KeywordsService {
 
-  constructor() { }
+  readonly ROOT_URL: 'https://localhost:44313/api/PBScrape';
+  private messageSource = new BehaviorSubject({});
+  currentMessage = this.messageSource.asObservable();
+
+  constructor(private http: HttpClient) { }
+  getAll(){
+    return this.http.get(ROOT_URL);
+  }
 }
